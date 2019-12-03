b4e187f72d15517241642dc5201314cc20c24dd3
petergiromagny <peter.giromagny@viacesi.fr>
2019-12-03 13:27:24 +0100

    Products page front OK

--- application/views/pages/products.php
+++ application/views/pages/products.php
@@ -1,10 +1,18 @@
 <div class="row">
 <?php if (isset($product)) {
 	foreach ($product as $products) : ?>
-			<div class="col-md-4" style="height: 400px; margin-bottom: 40px">
-				<div class="mb-4 shadow-lg" style="height: 100%;">
-					<h3><?= $products['name'] ?></h3>
-					<img class="img-product" src="<?= $products['img'] ?>" alt="<?= $products['name'] ?>">
+			<div class="col-md-4">
+				<div class="mb-4 shadow-lg block-product" style="margin-bottom: 2rem !important;">
+					<div class="block-title">
+						<h3><?= $products['name'] ?></h3>
+					</div>
+					<div class="block-img">
+						<img class="img-product" src="<?= $products['img'] ?>" alt="<?= $products['name'] ?>">
+					</div>
+					<div class="block-button">
+						<button type="button" class="btn btn-warning">See more</button>
+						<button type="button" class="btn btn-warning">Add to cart</button>
+					</div>
 				</div>
 			</div>
 	<?php endforeach;
