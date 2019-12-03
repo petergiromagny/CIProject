780264b8a440c7cc30236a07b790b87636a57a50
petergiromagny <peter.giromagny@viacesi.fr>
2019-12-03 10:25:42 +0100

    products design

--- application/views/pages/products.php
+++ application/views/pages/products.php
@@ -1,8 +1,13 @@
+<div class="row">
 <?php if (isset($product)) {
 	foreach ($product as $products) : ?>
-		<div>
-			<h3><?= $products['name'] ?></h3>
-			<img src="<?= $products['img'] ?>" alt="<?= $products['name'] ?>">
-		</div>
+			<div class="col-md-4" style="height: 400px; margin-bottom: 40px">
+				<div class="mb-4 shadow-lg" style="height: 100%;">
+					<h3><?= $products['name'] ?></h3>
+					<img class="img-product" src="<?= $products['img'] ?>" alt="<?= $products['name'] ?>">
+				</div>
+			</div>
 	<?php endforeach;
 } ?>
+</div>
+
